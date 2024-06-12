511d139af7b232798e8b132bf2540887ff05f320
isahalarnke <isabellarnke@outlook.com>
2024-06-12 09:59:45 +0200

    Label und  Antickboxen

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # skeletonanatomy
 Three JS application for Computer graphics module, skeleton anatomy learning tool
+
+Quellensammlung:
+
+- Three.js Dokumentation: https://threejs.org/docs/index.html#manual/en/introduction/Loading-3D-models 08.Juni 2024
+- Creating text: https://threejs.org/docs/#manual/en/introduction/Creating-text 10.Juni 2024
+- CSS diplay setting: https://www.w3schools.com/cssref/pr_class_display.php 12. Juni 2024
+- CSS labels: https://www.htmlgoodies.com/css/css-labels-buttons-forms/ 12. Juni 2024
+- 
--- skeletonscript.js
+++ skeletonscript.js
@@ -46,13 +46,18 @@ function main() {
   function loadGLTFModel(modelPath, scene, parent) {
     const modelName = modelPath.split("/").pop().split(".")[0];
 
+    console.log(modelName)
+
     gltflLoader.load(
       modelPath,
       function (gltf) {
+
+        // Anpegasst an die Szene, damit das Skelett etwas weiter links steht
         gltf.scene.scale.set(2, 2, 2);
         gltf.scene.position.y -= 6;
         gltf.scene.position.x -= 3;
         gltf.scene.rotation.y = -Math.PI / 6;
+        gltf.scene.userData.modelKey = modelName; // Das Model verfügt dann über einen Key
 
         scene.add(gltf.scene);
         skeleton.push(gltf.scene);
@@ -97,17 +102,42 @@ function main() {
   //GUI Control
   var controls = new (function () {
     this.rotY = 0;
+    this.allBodyparts = true;
   })();
 
+  // GUI control zum Rotieren
   var gui = new dat.GUI();
   gui.add(controls, "rotY", 0, 2 * Math.PI);
 
+  // GUI control zum Kästchen anticken, demnach Objekt sichtbar oder unsichtbar
+  var guiVisibileControl = new dat.GUI();
+
   function createModelControl(name, model) {
     controls[name] = true;
-    gui.add(controls, name).name(name).onChange(function(value) {
+    guiVisibileControl.add(controls, name).name(name).onChange(function (value) {
       model.visible = value;
     });
+    controls.allVisible = Object.keys(models).every(key => models[key].visible);
+    gui.__controllers.forEach(function (controller) {
+      if (controller.property === 'allVisible') {
+        controller.updateDisplay();
+      }
+    });
   }
+  // Hier alle verbergen oder sichtbar machen
+
+  guiVisibileControl.add(controls, 'allBodyparts').name('All Body Parts').onChange(function (value) {
+    Object.keys(models).forEach(function (key) {
+      models[key].visible = value;
+      controls[key] = value;
+    });
+    guiVisibileControl.__controllers.forEach(function(controller) {
+      if (controller.property === key) {
+          controller.updateDisplay();
+      }
+  });
+});
+
 
   //Trackball Control mit der Maus
   var trackballControls = new THREE.TrackballControls(camera, canvas);
@@ -119,13 +149,14 @@ function main() {
 
 
   function mouseHover(event) {
-    
+
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
     raycaster.setFromCamera(mouse, camera);
 
     let intersects = raycaster.intersectObjects(Object.values(models), true);
+    const label = document.getElementById("label");
 
     if (intersects.length > 0) {
       let intersectedObject = intersects[0].object;
@@ -135,14 +166,21 @@ function main() {
         intersected.currentHex = intersected.material.color.getHex();
         intersected.material.color.setHex(0x800080); // Hover-Farbe momentan auf pink gesetzt
       }
+
+      label.style.display = "block";
+      label.style.left = (event.clientX + 10) + 'px';
+      label.style.top = (event.clientY + 10) + 'px';
+      label.textContent = intersected.parent.userData.modelKey || 'Incorrect';
+
     } else {
       if (intersected) intersected.material.color.setHex(intersected.currentHex);
       intersected = null;
+      label.style.display = "none";
     }
   }
 
 
-  
+
   function animate() {
     if (resizeGLToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
